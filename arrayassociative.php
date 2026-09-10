2d1ba52642467ff2c18043900fa762f87265625b
Maureen Alya <maureencans08@gmail.com>
2026-09-10 09:16:19 +0700

    Add files via upload

---
+++ arrayassociative.php
@@ -0,0 +1,59 @@
+<?php
+
+$datasiswa = [
+
+    "nama_data_siswa" => "Maureen",
+    "Nis_data_siswa" => "0001",
+    "Kelas_data_siswa" => "XI PPLG 2",
+    "Hobi_data_siswa" => "Bermain Game",
+
+    "nama_data_siswa2" => "Silvi",
+    "Nis_data_siswa2" => "0002",
+    "Kelas_data_siswa2" => "XI PPLG 2",
+    "Hobi_data_siswa2" => "memasak",
+
+    "nama_data_siswa3" => "Fadil",
+    "Nis_data_siswa3" => "0003",
+    "Kelas_data_siswa3" => "XI PPLG 2",
+    "Hobi_data_siswa3" => "basket"
+];
+
+echo "Data nama siswa: " , $datasiswa["nama_data_siswa"];
+echo "<br/>";
+
+echo "Data Nis siswa: " , $datasiswa["Nis_data_siswa"];
+echo "<br/>";
+
+echo "Data Kelas siswa: " , $datasiswa["Kelas_data_siswa"];
+echo "<br/>";
+
+echo "Hobi nama siswa: " , $datasiswa["Hobi_data_siswa"];
+echo "<br/>";
+
+echo "<br/>";
+
+echo "Data nama siswa 2: " , $datasiswa["nama_data_siswa2"];
+echo "<br/>";
+
+echo "Data Nis siswa 2: " , $datasiswa["Nis_data_siswa2"];
+echo "<br/>";
+
+echo "Data Kelas siswa 2: " , $datasiswa["Kelas_data_siswa2"];
+echo "<br/>";
+
+echo "Hobi nama siswa 2: " , $datasiswa["Hobi_data_siswa2"];
+echo "<br/>";
+
+echo "<br/>";
+
+echo "Data nama siswa 3: " , $datasiswa["nama_data_siswa3"];
+echo "<br/>";
+
+echo "Data Nis siswa 3: " , $datasiswa["Nis_data_siswa3"];
+echo "<br/>";
+
+echo "Data Kelas siswa 3: " , $datasiswa["Kelas_data_siswa3"];
+echo "<br/>";
+
+echo "Hobi nama siswa 3: " , $datasiswa["Hobi_data_siswa3"];
+echo "<br/>";
